40d6b0ad8542b5a5400fc016f14006df4130c655
Ivanov Dan <danis.potcha@mail.ru>
2021-12-11 20:37:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число чтобы выйграть: ");
+            Console.ReadLine();
+            Console.WriteLine("Вы проиграли!");
+        }
+    }
+}
